c8d65acd4bb85f3dd12c613949448610e5a298ac
geirksa <geirksa@ifi.uio.no>
2024-11-30 22:54:36 +0200

    Simplified, cleaned, added readme

--- predict.r
+++ predict.r
@@ -1,40 +1,26 @@
 # Define the predict function
 ##
 predict_chap <- function(model_fn, historic_data_fn, future_climatedata_fn, predictions_fn) {
-  # Read the future climate data
   df <- read.csv(future_climatedata_fn)
-
-  # Extract features
   X <- df[, c("rainfall", "mean_temperature"), drop = FALSE]
-
-  # Load the trained model
   model <- readRDS(model_fn)  # Assumes the model was saved using saveRDS
 
-  # Make predictions
-  #browser()
   y_pred <- predict(model, newdata = X)
-
-  # Add predictions to the dataframe
   df$sample_0 <- y_pred
-
-  # Save predictions to a CSV file
   write.csv(df, predictions_fn, row.names = FALSE)
 
-  # Print predictions
-  print(paste("Predict - forecast values:", paste(y_pred, collapse = ", ")))
+  print(paste("Forecasted values:", paste(y_pred, collapse = ", ")))
 }
 
-# Main logic to handle command-line arguments
 args <- commandArgs(trailingOnly = TRUE)
 
 if (length(args) == 4) {
-#  stop("Usage: Rscript predict.R <model_fn> <historic_data_fn> <future_climatedata_fn> <predictions_fn>")
-  
   model_fn <- args[1]
   historic_data_fn <- args[2]
   future_climatedata_fn <- args[3]
   predictions_fn <- args[4]
   
-  # Call the predict function with the arguments
-  predict(model_fn, historic_data_fn, future_climatedata_fn, predictions_fn)
-}
+  predict_chap(model_fn, historic_data_fn, future_climatedata_fn, predictions_fn)
+}# else {
+#  stop("Usage: Rscript predict.R <model_fn> <historic_data_fn> <future_climatedata_fn> <predictions_fn>")
+#}
--- train.r
+++ train.r
@@ -1,43 +1,22 @@
-#library(argparse)
 library(caret)  # For model training
 library(readr)  # For reading CSV files
 library(broom)  # For extracting model coefficients
 library(tools)  # For file operations like saving the model
-##
+
 train_chap <- function(csv_fn, model_fn) {
-  # Read the data
   df <- read_csv(csv_fn)
-
-  # Print all column names (if desired)
-  print(names(df))
-
-  # Define features and response variable
-  features <- c('rainfall', 'mean_temperature')
-  #X <- df[, features, drop = FALSE]
-  #Y <- df$disease_cases
-
-  # Handle NA values in Y
   df$disease_cases[is.na(df$disease_cases)] <- 0
-  #browser()
-  # Train the linear regression model
   model <- lm(disease_cases ~ rainfall + mean_temperature, data = df)
-
-  # Save the model
   saveRDS(model, file=model_fn)
-
-  # Print the coefficients
-  #coef_df <- tidy(model)
-  #print(paste("Train - model coefficients:", paste(coef_df$term, coef_df$estimate, sep = "=", collapse = ", ")))
 }
-
 
 args <- commandArgs(trailingOnly = TRUE)
 
 if (length(args) == 2) {
-#  stop("Usage: Rscript train.R <csv_fn> <model_fn>")
-
   csv_fn <- args[1]
   model_fn <- args[2]
 
-  train(csv_fn, model_fn)
-}
+  train_chap(csv_fn, model_fn)
+}# else {
+#  stop("Usage: Rscript train.R <csv_fn> <model_fn>")
+#}
